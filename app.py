4ca5d3ffe5d1216a18b1f7af67971d5dba0cc05f
Ana Laura Diedrichs <analauradiedrichs@gmail.com>
2024-06-25 00:23:11 -0300

    delete token update models

--- app.py
+++ app.py
@@ -23,27 +23,12 @@ def download_text(url):
 
 from langchain.embeddings import HuggingFaceEmbeddings
 
-def get_embedding_function(embedding_name):
-  # Create a dictionary with model configuration options, specifying to use the CPU for computations
-  #model_kwargs = {'device':'cuda'}
-
-  # Create a dictionary with encoding options, specifically setting 'normalize_embeddings' to False
-  encode_kwargs = {'normalize_embeddings': True}
-
-  # Initialize an instance of HuggingFaceEmbeddings with the specified parameters
-  embeddings = HuggingFaceEmbeddings(
-      model_name=embedding_name,     # Provide the pre-trained model's path
-      #model_kwargs=model_kwargs, # Pass the model configuration options
-      encode_kwargs=encode_kwargs # Pass the encoding options
-  )
-  return embeddings
-
 documents = [download_text(url) for url in links]
 
 # Crear embeddings y base de datos vectorial
 
-model_name = "hiiamsid/sentence_similarity_spanish_es"
-embedding_model = get_embedding_function(model_name)
+embedding_model = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large")
+
 
 # Convertir documentos a embeddings
 #document_embeddings = embedding_model.encode(documents)
@@ -53,9 +38,24 @@ faiss_index = FAISS.from_texts(texts=documents, embedding=embedding_model)
 
 
 # Configurar el modelo LLM
-#"meta-llama/Llama-2-13b-hf"
-llm_model_name = "timpal0l/mdeberta-v3-base-squad2" # "meta-llama/Llama-2-13b-hf" #"nvidia/Llama3-ChatQA-1.5-8B"
-llm_pipeline = pipeline("question-answering", model=llm_model_name, token= "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" ) # st.secrets["HF_TOKEN"])
+from huggingface_hub.hf_api import HfFolder
+
+HfFolder.save_token('') #INTRODUCIR TOKEN DE HUGGINGFACE. DEBE ESTAR APROBADA LA SOLICITUD DE USO DE MODELO LLAMA-3
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+llm_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 # Crear la aplicación Streamlit
 st.title("Aplicación RAG con LLM en Español")
